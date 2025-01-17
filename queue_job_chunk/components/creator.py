akretion/sale-import

[IMP] Logic on queue.job.chunk, company_ids, tests, and component separation

queue_job_chunk/components/creator.py
@@ -1,0 +1,19 @@
+#  Copyright (c) Akretion 2020
+#  License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+
+import json
+
+from odoo.addons.component.core import Component
+
+
+class Creator(Component):
+    """ Just creates a record with given vals """
+
+    _inherit = "base"
+    _name = "basic.creator"
+    _collection = "queue.job.chunk"
+    _usage = "basic_create"
+
+    def run(self, data):
+        data = json.loads(data)
+        return self.model.create(data)

akretion/sale-channel

queue_job_chunk/components/creator.py
@@ -1,0 +1,18 @@
+#  Copyright (c) Akretion 2020
+#  License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+
+import json
+
+from odoo.addons.component.core import Component
+
+
+class Creator(Component):
+    """ Just creates a record with given vals """
+
+    _inherit = "processor"
+    _name = "basic.creator"
+    _usage = "basic_create"
+
+    def run(self, data):
+        data = json.loads(data)
+        return self.model.create(data)